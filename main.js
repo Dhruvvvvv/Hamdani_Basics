6a674b516a802ad2776194677437409bb2303189
Dhruvvvvv <shahan.sa98@gmail.com>
2020-07-20 17:59:08 +0530

    First commit.

---
+++ main.js
@@ -0,0 +1,11 @@
+function log(message) {
+    console.log(message);
+}
+var msg = "Hello TypeScript";
+log(msg);
+function doSomething() {
+    for (var i = 0; i > 5; i++) {
+        console.log(i);
+    }
+}
+doSomething();
